d788de3a0a99bca6196addef970a1585f3b66af8
DANIELTELLEZPRADA <31336271+DANIELTELLEZPRADA@users.noreply.github.com>
2019-06-16 13:11:10 -0500

    Teca-inverciones
    Teca inverciones

---
+++ teca.php
@@ -0,0 +1,410 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Diseño de paginas web profecionales</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Playball&display=swap" rel="stylesheet">
+</head>
+<body>
+<!--Estilos generales___________________________________________________________________________-->
+	<style type="text/css">
+		h2{
+			font-family: 'Playball', cursive;		
+		}
+		h1{
+			font-family: 'Playball', cursive;
+		}
+
+	</style>
+
+<!--Navegacion __________________________________________________________________-->
+
+<style type="text/css">
+	
+#logo{
+	width:20%;
+
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #000;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+
+
+</style>
+
+<nav>    	    
+
+<ul>
+  <li><a class="navbar-brand" href="teca.php"><img id="logo" src="img\05.png" ></a></li>
+  <li><a href="index.php">Inicio</a></li>
+  <li><a href="#news">Quienes somos</a></li>
+  <li><a href="#contact">Servicios </a> </li>
+  <li><a href="#about">Contactos</a></li>
+  <li><a href="#about">Telefono - 3148614637</a></li>
+  <li><a href="#about">WhatsAp- 3148614637</a></li>
+  <li><a href="https://www.facebook.com/TeCa.Santandercito/?ref=bookmarks">Facebook</a></li>
+</ul>   
+</nav>
+
+
+
+
+<!--_____________________________________________________________________________-->
+
+
+<!--Carrusel__________________________________________________________________-->
+
+<style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 100%;
+    height:500px;
+  }
+
+  </style>
+</head>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img\01.png">
+      <div class="carousel-caption" class="imgCarrusel">
+        <h3>Tienda virtual</h3>
+        <p></p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="img\02.png">
+      <div class="carousel-caption">
+        <h3>Mantenimiento de computadores</h3>
+        <p></p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="img\03.png">
+      <div class="carousel-caption">
+        <h3>Diseño WEB</h3>
+        <p></p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<!--_____________________________________________________________________________________________________________________________________-->
+<!--Franja azul ______________________________________________________________________________________-->
+<div style="height: 10px; background:    "></div>
+	
+
+
+
+<!--Como comprar______________________________________________________________________________________________________-->
+
+<style type="text/css">
+	.cuadroPasos{
+		background:#000;
+		margin-top:5%;
+		margin-left: 1%;
+		margin-right: 1%;
+		margin-bottom: 5%;
+		color:#fff;
+		text-align: center;
+	}
+</style>
+
+<center>
+<h1>TIENDA VIRTUAL - TeCa.</h1>
+	
+
+</center>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm cuadroPasos">
+      <h2>1</h2>
+      <p>Seleccione una de nuestras tiendas de confianza, elija los productos que necita.</p>
+    </div>
+    <div class="col-sm cuadroPasos">
+       <h2>2</h2>
+      <p>Acérquese a la tienda virtual TeCa, más cercana y realice su compra con la ayuda de nuestro asesor, realice el pago, y ya está.</p>
+    </div>
+    <div class="col-sm cuadroPasos">
+       <h2>3</h2>
+      <p>Una vez el producto llegue a la tienda, se le notificara vía mensaje de texto o a través de una llamada, para acordar la entrega, ya sea que recoja  su producto en la tienda o se lo podemos llevar a su casa.</p>
+  </div>
+</div>
+
+<!--Tiendas - TeCa____________________________________________________________________________________________-->
+
+<style type="text/css">
+	a p{
+		background: #000;
+		color: #fff;
+	}
+</style>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+    	<center>
+      <h2>TeCa - Santandercito</h2>
+      	<a href=""><p>Llamar: 3148614637</p></a>
+      	<a href=""><p>Whatsapp: 3148614637</p></a>
+      	<a href=""><p>Facebook: 3148614637</p></a>
+      	<iframe src="https://www.google.com/maps/embed?pb=!4v1560588463814!6m8!1m7!1szcdRwRoOdYKAaJCP0pMmjQ!2m2!1d4.594984655373211!2d-74.34199565382694!3f98.04974739294109!4f1.1335863236795092!5f0.7820865974627469" width="200" height="200" frameborder="0" style="border:0" allowfullscreen></iframe></center>
+    </div>
+    <div class="col-sm">
+      
+      <center>
+      	<h2>TeCa - Santandercito</h2>
+      	<a href=""><p>Llamar: 3148614637</p></a>
+      	<a href=""><p>Whatsapp: 3148614637</p></a>
+      	<a href=""><p>Facebook: 3148614637</p></a>
+      	<iframe src="https://www.google.com/maps/embed?pb=!4v1560588463814!6m8!1m7!1szcdRwRoOdYKAaJCP0pMmjQ!2m2!1d4.594984655373211!2d-74.34199565382694!3f98.04974739294109!4f1.1335863236795092!5f0.7820865974627469" width="200" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+      	
+      	
+      </center>
+    </div>
+    <div class="col-sm">
+    	<center>
+      <h2>TeCa - Santandercito</h2>
+      	<a href=""><p>Llamar: 3148614637</p></a>
+      	<a href=""><p>Whatsapp: 3148614637</p></a>
+      	<a href=""><p>Facebook: 3148614637</p></a>
+      	<iframe src="https://www.google.com/maps/embed?pb=!4v1560588463814!6m8!1m7!1szcdRwRoOdYKAaJCP0pMmjQ!2m2!1d4.594984655373211!2d-74.34199565382694!3f98.04974739294109!4f1.1335863236795092!5f0.7820865974627469" width="200" height="200" frameborder="0" style="border:0" allowfullscreen></iframe></center>
+    </div>
+  </div>
+</div>
+
+<!--Tiendas recmendadas ____________________________________________________________________-->
+<style type="text/css">
+	#tiendasRecomendads{
+		margin-top:1%;
+		font-family: 'Playball', cursive;
+		color:#fff;
+		margin-bottom:2%;
+	}
+.imagenesTiendasR{
+
+	width: 100%;
+	height: 100px;
+	margin-bottom: 2%;
+}
+#divTiendasRecomendadas{
+background:#1A5276;
+margin-bottom:1%;
+padding:1%;
+}
+</style>
+
+<div id="divTiendasRecomendadas">
+<center>
+<h2 id="tiendasRecomendads">Tiendas recomendadas</h2>
+</center>
+
+<div class="container">
+  <div class="row">
+  	<div class="col-sm">
+      <a href=""><img class="imagenesTiendasR"src="img\12.png"></a>
+    </div>
+    <div class="col-sm">
+      <a href=""><img class="imagenesTiendasR"src="img\14.png"></a>
+    </div>
+    <div class="col-sm">
+      <a href=""><img class="imagenesTiendasR"src="img\15.png"></a>
+    </div>
+    <div class="col-sm">
+      <a href=""><img class="imagenesTiendasR"src="img\16.png"></a>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+<!-- Chat y whatsapp__________________________________________________________________________-->
+
+ <!--Programacion  mensages mss Js_____________________________________-->
+<script>
+$(document).ready(function(){
+  $(".chat_boton").click(function(){
+    $(".iframe_mssfk").slideToggle();
+  });
+});
+</script>
+<!--Facebook  watsapp______________________________________-->
+<section class="chat_container" style="margin:0;
+  width:auto;
+  max-width:300px;
+  position:fixed;
+  bottom:0px;
+  right:2%;
+  z-index:999;">
+<center>
+<!-- watsapp______________________________________-->
+<div class="">
+    <a href="https://wa.me/573148614637" target="_blank"> <img id="fotoTelefono" src="https://image.flaticon.com/icons/svg/174/174879.svg" class="img_wsp" alt="" style="width:30%;
+  hyphens: ;"></a>      
+</div>
+<!--Facebook______________________________________-->
+<div class="chat_boton" style="width: 100%;   
+  cursor:pointer;
+  user-select: none;
+  padding:4px 0;">
+<img id="img_mss" src="https://image.flaticon.com/icons/svg/906/906386.svg" style="width:40%;
+hyphens:;">
+</div>
+<div class="iframe_mssfk" style="margin:0;
+padding:0;
+display: none;">
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FTeCa.Santandercito%2F%3Fref%3Dbookmarks&tabs=messages&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+</div>
+ </center>
+
+</section>
+
+
+
+
+
+<!--Pie de pagina______________________________________________________________________________-->
+
+<style type="text/css">
+
+	footer{
+		background:#000;
+		height: 500px;
+		color: #9A7D0A;
+		padding-top: 5%;
+		font-family: 'Playball', cursive;
+	}
+	
+	footer img{
+
+		width: 20%;
+
+	}
+
+</style>
+
+<footer>
+
+	<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm">
+      <center>
+      	<img src="img\05.png">
+      </center>
+    </div>
+    <div class="col-sm">
+    	<center>
+    		<p>Hola</p>
+    	</center>
+
+      
+    </div>
+    <div class="col-sm">
+      <center>
+      	<p>como estan</p>
+      </center>
+    </div>
+  </div>
+</div>
+	
+
+
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
